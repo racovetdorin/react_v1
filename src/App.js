001e124e22c6d348f730fd101e0ea5ba7174acd4
racovetdorin <71522263+racovetdorin@users.noreply.github.com>
2021-02-08 17:46:25 +0200

    let's try 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save  to reload.
         </p>
         <p>text in here</p>
-        <p>this is the update</p>
+        <p>this is the updatesdsd</p>
         <a
           className="App-link"
           href="https://reactjs.org"
